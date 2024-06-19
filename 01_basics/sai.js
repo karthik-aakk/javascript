2c466a45da7f37c4a4b27f4ab801935261c3cffa
karthik-aakk <akmaddula123@gmail.com>
2024-06-19 20:24:59 +0530

    added 2 new files

---
+++ 01_basics/sai.js
@@ -0,0 +1,11 @@
+// function addition(a, b) {
+//     return a + b;
+// }
+// //addition(1, 2);
+// console.log(addition(1, 2));
+
+function addition(a, b) {
+    console.log(a + b);
+}
+
+addition(1, 2);
